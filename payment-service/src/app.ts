bddd3c70cd26931ec051a5e711022f670becb7e5
Chouaib <chouaib.skt@gmail.com>
2025-04-09 12:25:05 +0200

    feat: enhance security with content security policy and serve static files

--- payment-service/src/app.ts
+++ payment-service/src/app.ts
@@ -4,17 +4,33 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import connectDB from './config/db';
 import paymentRoutes from './routes/payment.routes';
+import path from 'path';
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "https://js.stripe.com"], // ⚠️ 'unsafe-inline' is risky
+        styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+      }
+    })
+  );
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/payments', paymentRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  });
+  
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
