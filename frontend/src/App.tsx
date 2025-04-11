d7fddf2744121b91ad33e525947210b4c46c974e
Chouaib <chouaib.skt@gmail.com>
2025-04-11 19:51:55 +0200

    refactor: clean up App component by removing commented-out code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,36 @@
-// import React from 'react';
-// import { loadStripe } from '@stripe/stripe-js';
-// import { Elements } from '@stripe/react-stripe-js';
-// import PaymentForm from './components/forms/payment-form';
-// import './App.css';
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import PaymentForm from './components/PaymentForm';
+import './App.css';
 
-// const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
-
-// function App() {
-//   return (
-//     <Elements stripe={stripePromise}>
-//       <div className="app">
-//         <PaymentForm />
-//       </div>
-//     </Elements>
-//   );
-// }
-
-// export default App;
-
-
-import PaymentForm from "./components/forms/payment-form/payment-form"
-import "./App.css"
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 function App() {
   return (
-    <div className="app">
-      <PaymentForm />
-    </div>
-  )
+    <Elements stripe={stripePromise}>
+      <div className="app">
+        <PaymentForm />
+      </div>
+    </Elements>
+  );
 }
 
-export default App
+export default App;
+
+
+// import PaymentForm from "./components/forms/payment-form/payment-form"
+// import "./App.css"
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <PaymentForm />
+//     </div>
+//   )
+// }
+
+// export default App
 
 
 
